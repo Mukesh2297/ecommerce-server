2dd7850fcbeaeb5c70bca40810529e9c02e74954
Mukesh Raj <mukesh.rajasekar@gmail.com>
2020-11-29 15:23:11 +0530

    controller changes

--- routes/feed.js
+++ routes/feed.js
@@ -3,11 +3,15 @@ const feedController = require("../controllers/feed");
 const router = express.Router();
 
 
-router.get("/getProducts",feedController.getProducts);
+router.get("/getProducts", feedController.getProducts);
 
-router.get("/",(req,res)=>{
-    res.status(200).json({status:'Ok'})
+router.post("/filterbycategory", feedController.filterByCategory);
+
+router.get("/", (req, res) => {
+    res.status(200).json({ status: 'Ok' })
 });
+
+
 
 
 
